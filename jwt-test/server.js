c02a0d5d6a9381b9af1ffe18aa394be5bc4709a7
jcarlo123 <jcg.ricablanca@gmail.com>
2020-08-28 20:26:47 +0800

    jwt test

---
+++ jwt-test/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username: 'jj',
+        title: 'post 1'
+    },
+    {
+        username: 'jo',
+        title: 'post 2'
+    }
+]
+
+app.get('/posts', (req, res) => {
+    res.json(posts);
+})
+
+app.post('/login', (req, res) => {
+    //Authenticate user
+
+    const username = req.body.username
+    const user = { username }
+
+    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+})
+
+app.listen(3000)
